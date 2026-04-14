3bbab0d317aac7b9109bf36b7b9d7b0ec438526a
Wahid Nurhisyam <ichamdaiboz@gmail.com>
2026-04-14 11:38:25 +0700

    Update next.config.ts

--- client-side/next.config.ts
+++ client-side/next.config.ts
@@ -5,9 +5,7 @@ const nextConfig: NextConfig = {
     root: process.cwd(),
   },
 
-  experimental: {
-    serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
-  },
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
 
   // Allow Google profile images + ImageKit
   images: {
